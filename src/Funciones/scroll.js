7df8b6cd8bebb3cda86bedd27f50b1b54e797f6b
david.machio <david.machio@MB-07.local>
2024-04-01 03:09:05 +0200

    finito

--- src/Funciones/scroll.js
+++ src/Funciones/scroll.js
@@ -1,4 +1,5 @@
 import { inputValue } from "../components/Header/Header";
+import { printProfile } from "../components/Profile/Profile";
 import { printInicio } from "./inicio";
 import { primeraBusqueda, printBusqueda } from "./printBusqueda";
 
@@ -9,25 +10,29 @@ export let page = 1
 
 
 export const printScroll = async () => {
-    window.addEventListener('scroll', function() {
-        if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
-            console.log('Se ha llegado al final de la página');
-            if(inputValue){ 
-                console.log('1')
-                if(primeraBusqueda === false){
-                    console.log('2')
-                    page++
-                printBusqueda(inputValue, page++)
-                }else{
-                    console.log('3')
+    if(printProfile){
+        return
+    }else{
+        window.addEventListener('scroll', function() {
+            if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
+                console.log('Se ha llegado al final de la página');
+                if(inputValue){ 
+                    console.log('1')
+                    if(primeraBusqueda === false){
+                        console.log('2')
+                        page++
                     printBusqueda(inputValue, page++)
+                    }else{
+                        console.log('3')
+                        printBusqueda(inputValue, page++)
+                    }
+                }else{
+                    console.log('4')
+                    console.log('segunda condicion')
+                    printInicio()
                 }
-            }else{
-                console.log('4')
-                console.log('segunda condicion')
-                printInicio()
+               
             }
-           
-        }
-    })
+        })
+    }
 }
